daa1c1766424adc440aba5b6cfa213abade41226
ThahaniHussain <tahanihussain4@gmail.com>
2025-09-30 12:06:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/header"
+import About from "./components/about"
+import Blog from "./components/blog"
+import Footer from "./components/footer"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+export default App
